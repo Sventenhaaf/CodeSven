3f3dfd96a7f55961a345c70bcd8611f0ef7b0499
Sven ten Haaf <sventenhaaf@hotmail.com>
2015-10-14 22:44:50 -0700

    added css and starting submit button functionality

--- app/assets/javascripts/components/create/create.js.jsx
+++ app/assets/javascripts/components/create/create.js.jsx
@@ -7,9 +7,16 @@ var SnippetCreate = React.createClass({
       <div className="container snippetcreator">
         <div className="row stretch-height">
           <div className="col-md-6 stretch-height">
-            <textarea class="form-control" rows="20"></textarea>
+            <form className="form-horizontal">
+              <div className="form-group">
+                <label for="codeInput" class="col-sm-2 control-label">Type your code here</label>
+                <textarea id="codeInput" className="form-control" rows="3"></textarea>
+              </div>
+              <button type="submit" className="btn btn-default">Submit code</button>
+            </form>
           </div>
           <div className="col-md-6 stretch-height">
+            <strong>Result here</strong>
             <pre className="prettyprint stretch-height">{body} </pre>
           </div>
         </div>
